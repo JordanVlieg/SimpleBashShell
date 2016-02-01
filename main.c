40e1514ab11d91ddf67bd3a2bd2784a453bd07b4
Jordan Vlieg <jordanvlieg@gmail.com>
2016-01-31 20:45:04 -0800

    fixed entering null issue

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ struct Process
 {
 	int pid;
 	char command[1000];
+	char running;
 };
 
 void Exited_Process(int sig)
@@ -48,148 +49,207 @@ int main ( void )
 	{
 		bgList[item].pid = -2;
 	}
-	//char* bgNameList[5];
+
 	for (;;)
 	{
 		char* cmd = readline ("shell>");
-		char command[1000];
-		strncpy(command, cmd, 1000);
-		printf ("Got: [%s]\n", cmd);
-		int waitStatus;
-		char* theArgs[ARGSLEN];
-		const char delimiter[2] = " ";
-		char* token;
-		token  = strtok(cmd, delimiter);
-		int counter;
-		int bgFlag = 0;
-		for(counter = 0; token != NULL && counter < ARGSLEN - 1; counter++)
+		if(strcmp(cmd, ""))
 		{
-			theArgs[counter] = token;
-			token = strtok(NULL, delimiter);
-		}
-		theArgs[counter] = NULL;
-
-		if(strcmp(theArgs[0], "bg") == 0)
-		{
-			printf("reached\n");
-			bgFlag = 1;
-		}
-
-		if(strcmp(theArgs[bgFlag], "bglist") == 0)
-		{
-			int theJob = 0;
-			for(; theJob < 5; theJob++)
+			char command[1000];
+			strncpy(command, cmd, 1000);
+			printf ("Got: [%s]\n", cmd);
+			int waitStatus;
+			char* theArgs[ARGSLEN];
+			const char delimiter[2] = " ";
+			char* token;
+			token  = strtok(cmd, delimiter);
+			int counter;
+			int bgFlag = 0;
+			for(counter = 0; token != NULL && counter < ARGSLEN - 1; counter++)
 			{
-				if(bgList[theJob].pid != -2)
+				theArgs[counter] = token;
+				token = strtok(NULL, delimiter);
+			}
+			theArgs[counter] = NULL;
+
+			if(strcmp(theArgs[0], "bg") == 0)
+			{
+				printf("reached\n");
+				bgFlag = 1;
+			}
+
+			if(strcmp(theArgs[bgFlag], "bglist") == 0)
+			{
+				int theJob = 0;
+				for(; theJob < 5; theJob++)
 				{
-					printf("%d: %s\n", theJob, bgList[theJob].command);
+					if(bgList[theJob].pid != -2)
+					{
+						printf("%d: %s\n", theJob, bgList[theJob].command);
+					}
 				}
 			}
-		}
-		else if(strcmp(theArgs[bgFlag], "bgkill") == 0)
-		{
-			int process;
-			char switcher = theArgs[1+bgFlag][0];
-			if(switcher == '0')
+			else if(strcmp(theArgs[bgFlag], "bgkill") == 0 || strcmp(theArgs[bgFlag], "start") == 0 || strcmp(theArgs[bgFlag], "stop") == 0)
 			{
-				process = 0;
+				int process;
+				char switcher = theArgs[1+bgFlag][0];
+				if(switcher == '0')
+				{
+					process = 0;
+				}
+				else if(switcher == '1')
+				{
+					process = 1;
+				}
+				else if(switcher == '2')
+				{
+					process = 2;
+				}
+				else if(switcher == '3')
+				{
+					process = 3;
+				}
+				else if(switcher == '4')
+				{
+					process = 4;
+				}
+				else
+				{
+					process = -1;
+					printf("Invalid process number %d\n", switcher);
+				}
+
+				if(process >= 0)
+				{
+					if(strcmp(theArgs[bgFlag], "bgkill") == 0)
+					{
+						kill(bgList[process].pid, SIGTERM);
+						printf("Killed process: %d\n", process);
+						bgList[process].pid = -2;
+					}
+					else if(strcmp(theArgs[bgFlag], "start") == 0)
+					{
+						kill(bgList[process].pid, SIGCONT);
+						printf("Started process: %d\n", process);
+					}
+					else if(strcmp(theArgs[bgFlag], "stop") == 0)
+					{
+						kill(bgList[process].pid, SIGSTOP);
+						printf("Stopped process: %d\n", process);
+					}
+
+				}
+				
 			}
-			else if(switcher == '1')
+			else if(strcmp(theArgs[bgFlag], "stop") == 0)
 			{
-				process = 1;
+				int process;
+				char switcher = theArgs[1+bgFlag][0];
+				if(switcher == '0')
+				{
+					process = 0;
+				}
+				else if(switcher == '1')
+				{
+					process = 1;
+				}
+				else if(switcher == '2')
+				{
+					process = 2;
+				}
+				else if(switcher == '3')
+				{
+					process = 3;
+				}
+				else if(switcher == '4')
+				{
+					process = 4;
+				}
+				else
+				{
+					process = -1;
+					printf("Invalid process number %d\n", switcher);
+				}
+
+				if(process >= 0)
+				{
+					kill(bgList[process].pid, SIGTERM);
+					printf("Killed process: %d\n", process);
+					bgList[process].pid = -2;
+				}
+				
 			}
-			else if(switcher == '2')
+			else if(strcmp(theArgs[bgFlag], "cd") == 0)
 			{
-				process = 2;
-			}
-			else if(switcher == '3')
-			{
-				process = 3;
-			}
-			else if(switcher == '4')
-			{
-				process = 4;
+				if(theArgs[1] == NULL)
+				{
+					printf("No directory given\n");
+				}
+				else
+				{
+					char theDirectory[100];
+					getcwd(theDirectory, 100);
+					strcat(theDirectory, "/");
+					strcat(theDirectory, theArgs[1]);
+					int returnVal = chdir(theDirectory);
+					if(returnVal == -1)
+					{
+						printf("Error changing directory\n");
+					}
+				}
 			}
 			else
 			{
-				process = -1;
-				printf("Invalid process number %d\n", switcher);
-			}
-
-			if(process >= 0)
-			{
-				kill(bgList[process].pid, SIGTERM);
-				printf("Killed process: %d\n", process);
-				bgList[process].pid = -2;
-			}
-			
-		}
-		else if(strcmp(theArgs[bgFlag], "cd") == 0)
-		{
-			if(theArgs[1] == NULL)
-			{
-				printf("No directory given\n");
-			}
-			else
-			{
-				char theDirectory[100];
-				getcwd(theDirectory, 100);
-				strcat(theDirectory, "/");
-				strcat(theDirectory, theArgs[1]);
-				int returnVal = chdir(theDirectory);
-				if(returnVal == -1)
+				pid_t childpid = fork();
+				if(childpid == 0)
 				{
-					printf("Error changing directory\n");
+					// This is the child thread
+					execvp(theArgs[bgFlag], theArgs); //A very dirty dirty trick
+					printf("This is hit now\nbg ");
+					exit(EXIT_SUCCESS);
 				}
+				else if(childpid == -1)
+				{
+					// Fork was unsuccessful
+					printf("Internal system error: Fork\n");
+					exit(EXIT_FAILURE);
+				}
+				else
+				{
+					// This is the parent thread
+
+					if(bgFlag == 0)
+					{
+						waitpid(childpid, &waitStatus, 0);
+						printf("child finished\n");
+					}
+					else
+					{
+						int bgCounter = 0;
+						for(; bgCounter < 5; bgCounter++)
+						{
+							if(bgList[bgCounter].pid == -2)
+							{
+								printf("Added a process to the list: %s\n", cmd);							bgList[bgCounter].pid = childpid;
+								strncpy(bgList[bgCounter].command, command, 1000);
+								break;
+							}
+							else
+							{
+								printf("PID is: %d\n", bgList[bgCounter].pid);
+							}
+						}
+						printf("The child process ID is: %d\n", childpid);
+						signal(SIGCHLD, Exited_Process);
+					}
+
+				}
+				free (cmd);
 			}
 		}
 		else
 		{
-			pid_t childpid = fork();
-			if(childpid == 0)
-			{
-				// This is the child thread
-				execvp(theArgs[bgFlag], theArgs); //A very dirty dirty trick
-				printf("This is hit now\nbg ");
-				exit(EXIT_SUCCESS);
-			}
-			else if(childpid == -1)
-			{
-				// Fork was unsuccessful
-				printf("Internal system error: Fork\n");
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				// This is the parent thread
-
-				if(bgFlag == 0)
-				{
-					waitpid(childpid, &waitStatus, 0);
-					printf("child finished\n");
-				}
-				else
-				{
-					int bgCounter = 0;
-					for(; bgCounter < 5; bgCounter++)
-					{
-						if(bgList[bgCounter].pid == -2)
-						{
-							printf("Added a process to the list: %s\n", cmd);							bgList[bgCounter].pid = childpid;
-							strncpy(bgList[bgCounter].command, command, 1000);
-							break;
-						}
-						else
-						{
-							printf("PID is: %d\n", bgList[bgCounter].pid);
-						}
-					}
-					printf("The child process ID is: %d\n", childpid);
-					signal(SIGCHLD, Exited_Process);
-				}
-
-			}
-			free (cmd);
+			printf("Please enter a command\n");
 		}
 	}
 }
